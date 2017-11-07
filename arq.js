816dde152c0989e27e6fc491ab8105167fe1c0af
cleidialkimim <cleidialkimim@gmail.com>
2017-11-07 19:13:23 -0200

    Update arq.js

--- arq.js
+++ arq.js
@@ -1,9 +1,8 @@
 function soma () {
 var n1, n2, soma;
-n1 = document.getElementById ("primeiroNumero").value;
-n2 = document.getElementById ("segundoNumero").value;
-n1 = parseInt (n1);
-n2 = parseInt (n2);
+n1 = parseint(document.getElementById ("primeiroNumero").value);
+n2 = parseint(document.getElementById ("segundoNumero").value);
+
 soma = n1 + n2;
 document.getElementById ("resposta").innerHTML = soma;
 }
